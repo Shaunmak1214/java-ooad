f03f63a3755c02ae854de750a0491589aaddfc66
sinyin-ong <69614329+sinyin-ong@users.noreply.github.com>
2022-03-30 23:30:46 +0800

    fix: remove error message

--- DrawCircle.java
+++ DrawCircle.java
@@ -19,36 +19,26 @@ public class DrawCircle {
     }
 
     public void drawPoint(Graphics g) {
-        g.fillOval(xPoints[clickCount],yPoints[clickCount], 2, 2);
+        g.fillOval(xPoints[clickCount],yPoints[clickCount]+30, 2, 2);
 
         clickCount++;
     }
 
     public void drawShape(Graphics g) {
-        g.fillOval(xPoints[1],yPoints[1],2,2);
+        g.fillOval(xPoints[1],yPoints[1]+30,2,2);
 
-        if(xPoints[0] == xPoints[1]) {
-            ConstructGui.createPopUp("ErrorForXPlane");
-        }
+        int circWidth = (int)Math.sqrt(Math.pow(xPoints[1]-xPoints[0],2)+Math.pow(yPoints[1]-yPoints[0],2));
+        int r = circWidth/2;
+        int circMidptX = (int)((xPoints[0]+xPoints[1])/2);
+        int circMidptY = (int)((yPoints[0]+yPoints[1])/2)+30;
 
-        else if(yPoints[0] == yPoints[1]) {
-            ConstructGui.createPopUp("ErrorForYPlane");
-        }
-
-        else {
-            int circWidth = (int)Math.sqrt(Math.pow(xPoints[1]-xPoints[0],2)+Math.pow(yPoints[1]-yPoints[0],2));
-            int r = circWidth/2;
-            int circMidptX = (int)((xPoints[0]+xPoints[1])/2);
-            int circMidptY = (int)((yPoints[0]+yPoints[1])/2)+30;
-
-            System.out.println("circWidth: " + circWidth);
-            System.out.println("circMidptX: " + circMidptX);
-            System.out.println("circMidptY: " + circMidptY);
-            System.out.println("width: " + circWidth);
-            
-            g.drawOval(circMidptX-r,circMidptY-r, circWidth, circWidth);
-            
-            clickCount = 0;
-        }
+        System.out.println("circWidth: " + circWidth);
+        System.out.println("circMidptX: " + circMidptX);
+        System.out.println("circMidptY: " + circMidptY);
+        System.out.println("width: " + circWidth);
+        
+        g.drawOval(circMidptX-r,circMidptY-r, circWidth, circWidth);
+        
+        clickCount = 0;
     }
 }
